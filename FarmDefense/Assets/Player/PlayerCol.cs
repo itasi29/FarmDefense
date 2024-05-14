209ff489bd9916d7251aa3442e2e8a4d40da4282
ma-si- <Kouma9140@gmail.com>
2024-05-14 18:32:52 +0900

    inputSystemインストール

---
+++ FarmDefense/Assets/Player/PlayerCol.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//‚ ‚½‚Á‚½‚Æ‚«‚Ì‚µ‚å‚è‚Â‚­‚é‚æ
+public class PlayerCol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
