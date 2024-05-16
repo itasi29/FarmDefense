b941d095860c874d6bff425b6b98bbf117154b68
ma-si- <Kouma9140@gmail.com>
2024-05-16 19:00:45 +0900

    プレイヤーの動き作成

--- FarmDefense/Assets/Player/Player.cs
+++ FarmDefense/Assets/Player/Player.cs
@@ -1,19 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 //ÉvÉåÉCÉÑÅ[ÇÃèàóùÇ‹Ç∆ÇﬂÇÈÇÊ
 public class Player : MonoBehaviour
 {
+    private const float kSpeed = 0.2f;
+
+    private Vector3 _moveDir;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void FixedUpdate()
+    {
+        this.transform.position += _moveDir;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        Move();
+        Debug.Log(_moveDir);
+    }
+    private void Move()
+    {
         
+        Vector3 dirVec = new Vector3(0,0,0);
+
+        dirVec.x = Input.GetAxis("Horizontal");
+        dirVec.z = Input.GetAxis("Vertical");
+
+        dirVec.Normalize();
+
+        _moveDir = dirVec * kSpeed;
+
+        _moveDir.Normalize();
+
+
+        Debug.Log(_moveDir);
     }
 }
