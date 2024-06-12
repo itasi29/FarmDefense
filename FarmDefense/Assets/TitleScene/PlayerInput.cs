b20d4685d6ca321f017826cfa7d86914464e7385
GuniruPanjan <135081686+GuniruPanjan@users.noreply.github.com>
2024-06-12 18:54:09 +0900

    Sceneについてバグ発生中のため直す

--- FarmDefense/Assets/TitleScene/PlayerInput.cs
+++ FarmDefense/Assets/TitleScene/PlayerInput.cs
@@ -1,38 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class PlayerInput : MonoBehaviour
 {
- 
+    float lsh;
+    float lsv;
+    //float dph;
+    //float dpv;
+    Button end;
+    Button setting;
+    Button start;
+
+    GameObject A;
     void Start()
     {
-        
+        end = GameObject.Find("Canvas/GameObject/end/EndButton").GetComponent<Button>();
+        setting = GameObject.Find("Canvas/GameObject/setting/SettingButton").GetComponent<Button>();
+        start = GameObject.Find("Canvas/GameObject/start/StartButton").GetComponent<Button>();
+
+        start.Select();
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Tab)) //下キーを押す
+        //L Stick
+        lsh = Input.GetAxis("Horizontal");
+        lsv = Input.GetAxis("Vertical");
+
+        //D-Pad
+        //dph = Input.GetAxis("D_Pad_H");
+        //dpv = Input.GetAxis("D_Pad_V");
+
+        if ((lsh != 0) || (lsv != 0))
         {
-            OnSelected();
+            Debug.Log("L stick:" + lsh + "," + lsv);
         }
-        else if(Input.GetKeyUp(KeyCode.UpArrow))  //上キーを押す
+        //if ((dph != 0) || (dpv != 0))
+        //{
+        //    Debug.Log("D Pad:" + dph + "," + dpv);
+        //}
+
+        //startの上に行く場合
+        if (lsv > 0 && this == start)
         {
-
+            end.Select();
         }
-    }
-
-    public void OnSelected()
-    {
-        GameObject nowObj = EventSystem.current.currentSelectedGameObject; //選択中のオブジェクト取得
-
-        if (nowObj.TryGetComponent<Selectable>(out var sr))
+        //endの下に行く場合
+        if(lsv < 0 && this == end)
         {
-            Selectable mySelectable = sr;  //Selectableがある場合
-
-            EventSystem.current.SetSelectedGameObject(mySelectable.navigation.selectOnDown.gameObject); //一つ下のオブジェクト選択
+            start.Select();
         }
+
+        /*バグについて*/
+        //lsvが0から動くとになっているため、そのオブジェクト情報を取得した瞬間に瞬間移動のように選んでいるボタンが変わる
     }
 }
