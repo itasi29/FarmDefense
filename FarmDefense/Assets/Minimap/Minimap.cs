d2a40291eb6a4e78539034bf9b406931e3509317
ma-si- <Kouma9140@gmail.com>
2024-06-18 18:14:51 +0900

    プレイヤーとファーム表示できたんごごごごご

--- FarmDefense/Assets/Minimap/Minimap.cs
+++ FarmDefense/Assets/Minimap/Minimap.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Minimap : MonoBehaviour
 {
     [SerializeField] private Image FarmImage;
+    [SerializeField] private Image PlayerImage;
     //[SerializeField] private Image FarmImage2;
     //[SerializeField] private Image FarmImage3;
     //[SerializeField] private Image FarmImage4;
@@ -19,12 +21,14 @@ public class Minimap : MonoBehaviour
 
     private const int kMiniMapScale = 256;
 
-    private const int kMiniMapStartPosX = 352;
-    private const int kMiniMapStartPosY = 32;
+    private const int kMiniMapStartPosX = 480;
+    private const int kMiniMapStartPosY = 200;
 
+    private GameObject _canvas;
     private GameObject _player;
     private GameObject _farm;
-    private Image[] _farmImage = new Image[kFarmNum]; 
+    private Image[] _farmImage = new Image[kFarmNum];
+    private Image _playerImage;
     private Vector3[] _farmPos = new Vector3[kFarmNum];
     private Vector3 _playerPos;
     private Vector3[] _showFarmPos = new Vector3[kFarmNum];
@@ -32,10 +36,17 @@ public class Minimap : MonoBehaviour
     void Start()
     {
         _farm = GameObject.Find("Farm");
+        _canvas = GameObject.Find("Canvas");
         _player = GameObject.Find("Player");
         _playerPos = _player.transform.position;
         //Instantiate(FarmImage,,Quaternion.identity);
-        _farm.transform.parent = _player.transform;
+        for (int i = 0; i < kFarmNum; i++)
+        {
+            _farmImage[i] = Instantiate(FarmImage, new Vector3(1000, 1000, 1000), Quaternion.identity);
+            _farmImage[i].transform.parent = _canvas.transform;
+        }
+        _playerImage = Instantiate(PlayerImage,new Vector3(1000,1000,1000),Quaternion.identity);
+        _playerImage.transform.parent = _canvas.transform;
     }
 
     // Update is called once per frame
@@ -55,9 +66,17 @@ public class Minimap : MonoBehaviour
             _showFarmPos[i].x += kMiniMapStartPosX;
             _showFarmPos[i].y += kMiniMapStartPosY;
 
-            _farmImage[i].rectTransform.localPosition= _showFarmPos[i];
+            _farmImage[i].rectTransform.localPosition = _showFarmPos[i];
         }
         _playerPos = _player.transform.position;
+        //マップの大きさで座標を割り、全体の座標の割合を求める
+        _playerPos.x = (_playerPos.x / kMapH) * kMiniMapScale;
+        _playerPos.y = (_playerPos.z / kMapV) * kMiniMapScale;
+
+        _playerPos.x += kMiniMapStartPosX;
+        _playerPos.y += kMiniMapStartPosY;
+
+        _playerImage.rectTransform.localPosition = _playerPos;
 
     }
 }
