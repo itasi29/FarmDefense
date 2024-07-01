ab9cdd644f24af482ff2efec9c345bb6a8d15153
itasi29 <135081567+itasi29@users.noreply.github.com>
2024-07-01 12:41:57 +0900

    up

---
+++ FarmDefense/Assets/Script/Scene/TitleManager.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    /* 型 */
+    enum Next
+    {
+        kStart,
+        kOption,
+        kEnd,
+        kMax
+    }
+
+    /* 定数 */
+    [SerializeField] private const int kCursorHeight = 300;
+    private const string kNextSceneName = "UserSelectScene";
+
+
+    /* 変数 */
+    private int _index;
+    private bool _isPrePush;
+    private GameObject _cursor;
+
+    void Start()
+    {
+        _index = 0;
+        _isPrePush = false;
+    }
+
+    void Update()
+    {
+        CursorMove();
+
+        if (Input.GetButtonDown("A"))
+        {
+            if (_index == (int)Next.kStart)
+            {
+                // TODO: Sceneの切り替え(即時)
+                // TODO: fadeの終了時に着かえるように変更
+            }
+            else if (_index == (int)Next.kOption)
+            {
+                // TODO: キャンバス上に出すようにする
+            }
+            else if (_index == (int)Next.kEnd)
+            {
+#if UNITY_EDITOR
+                // エディター上の時
+                UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
+#else
+                // ビルド上の時
+                Application.Quit();//ゲームプレイ終了
+#endif
+            }
+        }
+    }
+
+    private void CursorMove()
+    {
+        float input = Input.GetAxis("DPADY");
+
+        if (!_isPrePush)
+        {
+            if (input == 1)
+            {
+                _index = ((int)Next.kMax + _index - 1) % (int)Next.kMax;
+            }
+            else if (input == -1)
+            {
+                _index = (_index + 1) % (int)Next.kMax;
+            }
+
+            _isPrePush = true;
+        }
+        else if (input == 0)
+        {
+            _isPrePush = false;
+        }
+    }
+}
