a44b3c3b5b88a2f8f8a9053f086ab4d42478ebba
ma-si- <Kouma9140@gmail.com>
2024-07-25 18:56:30 +0900

    ミニマップに農場の体力表示

--- FarmDefense/Assets/Script/Minimap/Minimap.cs
+++ FarmDefense/Assets/Script/Minimap/Minimap.cs
@@ -1,5 +1,7 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Reflection;
 using TMPro;
 using Unity.VisualScripting;
@@ -16,7 +18,7 @@ public class Minimap : MonoBehaviour
     }
 
 
-    [SerializeField] private Image FarmImage;
+    [SerializeField] private Slider FarmSlider;
     [SerializeField] private Image PlayerImage;
     [SerializeField] private Image EnemyImage;
 
@@ -34,11 +36,14 @@ public class Minimap : MonoBehaviour
     private const int kMiniMapStartPosX = 480;
     private const int kMiniMapStartPosY = 200;
 
+    private const float kChangeColorFarmHpRate = 0.4f;
+
     private GameObject _canvas;
     private GameObject _player;
     private GameObject _farm;
 
-    private Image[] _farmImage = new Image[kFarmNum];
+    private Slider[] _farmHpSlider = new Slider[kFarmNum];
+    private Slider _farmTotalHPSlider;
     private Vector3[] _farmPos = new Vector3[kFarmNum];
 
     private List<EnemyInfo> _enemyList = new List<EnemyInfo>();
@@ -50,26 +55,19 @@ public class Minimap : MonoBehaviour
     void Start()
     {
         _farm = GameObject.Find("Farm");
+        _farmTotalHPSlider = GameObject.Find("FarmTotalHpBar").GetComponent<Slider>();
         _canvas = GameObject.Find("Canvas");
         _player = GameObject.Find("Player");
         _playerPos = _player.transform.position;
         //Instantiate(FarmImage,,Quaternion.identity);
+        //農場の画像を表示
         for (int i = 0; i < kFarmNum; i++)
         {
-            _farmImage[i] = Instantiate(FarmImage, new Vector3(1000, 1000, 1000), Quaternion.identity);
-            _farmImage[i].transform.SetParent(_canvas.transform);
-            _farmImage[i].transform.localScale = new Vector2(kFarmImageSize,kFarmImageSize);
-
+            _farmHpSlider[i] = Instantiate(FarmSlider, new Vector3(1000, 1000, 1000), Quaternion.identity);
+            _farmHpSlider[i].transform.SetParent(_canvas.transform);
+            _farmHpSlider[i].transform.localScale = new Vector2(kFarmImageSize,kFarmImageSize);
         }
-        _playerImage = Instantiate(PlayerImage, new Vector3(1000, 1000, 1000), Quaternion.identity);
-        _playerImage.transform.SetParent(_canvas.transform);
-        _playerImage.transform.localScale = new Vector2(kPlayerImageSize,kPlayerImageSize);
-    }
-
-    // Update is called once per frame
-    [System.Obsolete]
-    void Update()
-    {
+        //農場のミニマップ座標設定
         for (int i = 0; i < kFarmNum; i++)
         {
             GameObject farm = _farm.transform.GetChild(i).gameObject;
@@ -84,8 +82,56 @@ public class Minimap : MonoBehaviour
             _farmPos[i].x += kMiniMapStartPosX;
             _farmPos[i].y += kMiniMapStartPosY;
 
-            _farmImage[i].rectTransform.localPosition = _farmPos[i];
+            _farmHpSlider[i].transform.localPosition = _farmPos[i];
+            _farmHpSlider[i].transform.localRotation = Quaternion.AngleAxis(90, Vector3.forward);
         }
+        _playerImage = Instantiate(PlayerImage, new Vector3(1000, 1000, 1000), Quaternion.identity);
+        _playerImage.transform.SetParent(_canvas.transform);
+        _playerImage.transform.localScale = new Vector2(kPlayerImageSize,kPlayerImageSize);
+    }
+
+    // Update is called once per frame
+    [System.Obsolete]
+    void Update()
+    {
+
+        float farmTotalHp = 0;
+        float farmTotalMaxHp = 0;
+        for (int i = 0; i < kFarmNum; i++)
+        {
+            Farm farmScript = _farm.transform.GetChild(i).gameObject.GetComponent<Farm>();
+
+            farmTotalHp += farmScript.Hp;
+            farmTotalMaxHp += farmScript.MaxHp;
+
+            float hpRate = (float)farmScript.Hp /(float)farmScript.MaxHp;
+
+            if(hpRate < 0)
+            {
+                hpRate = 0;
+            }
+
+            _farmHpSlider[i].value = hpRate;
+            //体力の割合が一定よりも下になったらスライダーの色を変える
+            if(hpRate < kChangeColorFarmHpRate)
+            {
+                Image sliderImage = _farmHpSlider[i].transform.GetChild(1).GetChild(0).GetComponent<Image>();
+
+                sliderImage.color = Color.yellow;
+            }
+        }
+
+        float farmTotalHpRate = farmTotalHp / farmTotalMaxHp;
+
+        _farmTotalHPSlider.value = farmTotalHpRate;
+
+        if (farmTotalHpRate < kChangeColorFarmHpRate)
+        {
+            Image sliderImage = _farmTotalHPSlider.transform.GetChild(1).GetChild(0).GetComponent<Image>();
+
+            sliderImage.color = Color.yellow;
+        }
+
         _playerPos = _player.transform.position;
         //マップの大きさで座標を割り、全体の座標の割合を求める
         _playerPos.x = (_playerPos.x / kMapH) * kMiniMapScale;
